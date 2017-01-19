ba55dc2f65bdae4b3c2f4b1f257845097e484d3f
Shinta Koyanagi <yancya@upec.jp>
2017-01-19 19:49:16 +0900

    Add a spec that can't grouping by Array

--- test/test_array.rb
+++ test/test_array.rb
@@ -11,4 +11,22 @@ class ArrayTest < Test::Unit::TestCase
       SQL
     end
   end
+
+  test "group by Array" do
+    msg = "invalidQuery: Grouping by expressions of type ARRAY is not allowed at [9:10]"
+    expected_error = Google::Cloud::InvalidArgumentError.new(msg)
+
+    assert_raise(expected_error) do
+      query <<~SQL
+        WITH t AS (
+      SELECT *
+        FROM UNNEST(ARRAY<STRUCT<id INT64, messages ARRAY<STRING>>>
+               [(1, ['a', 'b']), (2, ['c', 'd'])]))
+
+      SELECT messages, COUNT(id)
+        FROM t
+    GROUP BY 1
+      SQL
+    end
+  end
 end
